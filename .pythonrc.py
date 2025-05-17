d5fa0f5d4fc26a0f3707eb883436203c61cac786
Shalin Pather <shalin.pather@outlook.com>
2025-05-17 11:31:42 +0200

    Add Python RC file

---
+++ .pythonrc.py
@@ -0,0 +1,11 @@
+# Universal default imports (I use these all the time).
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Force UTF-8 (especially on Windows)
+import sys
+sys.stdin.reconfigure(encoding='utf-8')
+sys.stdout.reconfigure(encoding='utf-8')
+sys.stderr.reconfigure(encoding='utf-8')
+del sys
