a972b39f66337d1999871cb949a313d5ebd36475
Shalin Pather <shalin.pather@outlook.com>
2025-05-18 10:24:16 +0200

    Add clear_screen() command in Python profile

--- .pythonrc.py
+++ .pythonrc.py
@@ -3,9 +3,27 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 # Force UTF-8 (especially on Windows)
 import sys
-sys.stdin.reconfigure(encoding='utf-8')
-sys.stdout.reconfigure(encoding='utf-8')
-sys.stderr.reconfigure(encoding='utf-8')
+
+sys.stdin.reconfigure(encoding="utf-8")
+sys.stdout.reconfigure(encoding="utf-8")
+sys.stderr.reconfigure(encoding="utf-8")
+
 del sys
+
+
+# Another thing I seriously need to clear the screen.
+# "cls" is a keyword, and "clear" is too broad.
+import os
+import platform
+
+def clear_screen():
+    command = "cls" if "Windows" in platform.system() else "clear"
+    os.system(command)
+
+globals()["clear_screen"] = clear_screen
+
+del platform
+del os
