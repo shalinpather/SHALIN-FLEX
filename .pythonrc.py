7660d92b8f2133aae7dcf66aeb04911346cb356a
Shalin Pather <shalin.pather@outlook.com>
2025-05-19 16:11:06 +0200

    Remove non-functioning clear_screen() function

--- .pythonrc.py
+++ .pythonrc.py
@@ -12,18 +12,3 @@ sys.stdout.reconfigure(encoding="utf-8")
 sys.stderr.reconfigure(encoding="utf-8")
 
 del sys
-
-
-# Another thing I seriously need to clear the screen.
-# "cls" is a keyword, and "clear" is too broad.
-import os
-import platform
-
-def clear_screen():
-    command = "cls" if "Windows" in platform.system() else "clear"
-    os.system(command)
-
-globals()["clear_screen"] = clear_screen
-
-del platform
-del os
